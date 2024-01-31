c3dd4009d2177861d3d05c3be6aa844827d9ba08
Sergey Kiba <zzkibazz@ukr.net>
2024-01-31 21:02:13 +0100

    preparation for the 2nd lesson

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,13 +1,21 @@
-type TaskPropsType = {
+import { Button } from './Button';
+
+export type TaskPropsType = {
 	title: string;
 	isDone: boolean;
+	id: number;
+	removeTask?: (id: number) => void;
 };
 
-export function Task({ title, isDone }: TaskPropsType) {
+export function Task({ title, isDone, id, removeTask }: TaskPropsType) {
 	return (
 		<li>
 			<input type='checkbox' checked={isDone} />
 			<span>{title}</span>
+			<Button
+				onClick={() => removeTask && removeTask(id)}
+				title={'✖️'}
+			></Button>
 		</li>
 	);
 }
