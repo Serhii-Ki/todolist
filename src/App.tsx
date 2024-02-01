e88c632b9dcb1e3b7b29776fca35f8f0268fa995
Sergey Kiba <zzkibazz@ukr.net>
2024-02-01 18:37:46 +0100

    adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { v1 } from 'uuid';
 import './App.css';
 import ToDoList from './ToDoList';
 
 function App() {
 	let tasks1 = [
-		{ id: 1, title: 'HTML&CSS', isDone: true },
-		{ id: 2, title: 'JS', isDone: true },
-		{ id: 3, title: 'ReactJS', isDone: false },
-		{ id: 4, title: 'Rest API', isDone: false },
-		{ id: 5, title: 'GraphQL', isDone: false },
+		{ id: v1(), title: 'HTML&CSS', isDone: true },
+		{ id: v1(), title: 'JS', isDone: true },
+		{ id: v1(), title: 'ReactJS', isDone: false },
+		{ id: v1(), title: 'Rest API', isDone: false },
+		{ id: v1(), title: 'GraphQL', isDone: false },
 	];
 
 	return (
