55d0ada3539eea373c431016d3f4b1df4a22dedc
Sergey Kiba <serhiikiba@gmail.com>
2024-02-20 19:39:21 +0100

    added edit task

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { TasksType } from './App';
 import { Button } from './Button';
 
 export type TaskPropsType = {
 	removeTask?: (id: string) => void;
 	isCheckedHandler?: (id: string) => void;
+	changeTaskTitle?: (id: string, newTitle: string) => void;
 } & TasksType;
 
 export function Task({
@@ -12,15 +14,41 @@ export function Task({
 	id,
 	removeTask,
 	isCheckedHandler,
+	changeTaskTitle,
 }: TaskPropsType) {
+	const [inputEditText, setInputEditText] = useState<string>('');
+	const [activeInput, setActiveInput] = useState(false);
+
 	return (
-		<li className={isDone ? 'task-done' : ''}>
+		<li>
 			<input
 				type='checkbox'
 				checked={isDone}
 				onChange={() => isCheckedHandler && isCheckedHandler(id)}
 			/>
-			<span>{title}</span>
+			<span className={isDone ? 'task-done' : ''}>{title}</span>
+			<input
+				className={activeInput ? '' : 'input-edit input-non'}
+				type='text'
+				value={inputEditText}
+				onChange={e => setInputEditText(e.currentTarget.value)}
+			/>
+			<Button
+				className={activeInput ? '' : 'confirm-none'}
+				title='confirm'
+				onClick={() => {
+					changeTaskTitle && changeTaskTitle(id, inputEditText);
+					setInputEditText('');
+					setActiveInput(false);
+				}}
+			/>
+			<Button
+				className={activeInput ? 'edit-none' : ''}
+				title='&#9998;'
+				onClick={() => {
+					setActiveInput(true);
+				}}
+			/>
 			<Button
 				onClick={() => removeTask && removeTask(id)}
 				title={'✖️'}
