0caf4e0444eb3feeaa109d5ddd80a2b853f15722
Sergey Kiba <serhiikiba@gmail.com>
2024-03-17 11:53:52 +0100

    finished actions and reducer Todo

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,8 +1,18 @@
-export type ActionsTodoListType = RemoveTodoList | RemoveTaskType;
+import { FilterType } from "../pages/main/Main";
 
-type RemoveTodoList = ReturnType<typeof removeTodoList>;
+export type ActionsTodoListType =
+  | RemoveTodoList
+  | RemoveTaskType
+  | AddTodoListType
+  | AddTaskType
+  | ChangeCheckedType
+  | ChangeFilterType
+  | EditTodoListType
+  | EditTaskType;
 
-export const removeTodoList = (todoId: string) => {
+type RemoveTodoList = ReturnType<typeof removeTodoListAC>;
+
+export const removeTodoListAC = (todoId: string) => {
   return {
     type: "REMOVE-TODOLIST",
     payload: {
@@ -11,14 +21,86 @@ export const removeTodoList = (todoId: string) => {
   } as const;
 };
 
-type RemoveTaskType = ReturnType<typeof removeTask>;
+type RemoveTaskType = ReturnType<typeof removeTaskAC>;
 
-export const removeTask = (todoId: string, taskId: string) => {
+export const removeTaskAC = (todoId: string, taskId: string) => {
   return {
     type: "REMOVE-TASK",
     payload: {
       todoId,
       taskId,
     },
-  };
+  } as const;
+};
+
+type AddTodoListType = ReturnType<typeof addTodoListAC>;
+
+export const addTodoListAC = (title: string) => {
+  return {
+    type: "ADD-TODOLIST",
+    payload: {
+      title,
+    },
+  } as const;
+};
+
+type AddTaskType = ReturnType<typeof addTaskAC>;
+
+export const addTaskAC = (todoId: string, title: string) => {
+  return {
+    type: "ADD-TASK",
+    payload: {
+      todoId,
+      title,
+    },
+  } as const;
+};
+
+type ChangeCheckedType = ReturnType<typeof changeCheckedAC>;
+
+export const changeCheckedAC = (todoId: string, taskId: string) => {
+  return {
+    type: "CHANGE-CHECKED",
+    payload: {
+      todoId,
+      taskId,
+    },
+  } as const;
+};
+
+type ChangeFilterType = ReturnType<typeof changeFilterAC>;
+
+export const changeFilterAC = (todoId: string, filter: FilterType) => {
+  return {
+    type: "CHANGE-FILTER",
+    payload: {
+      todoId,
+      filter,
+    },
+  } as const;
+};
+
+type EditTodoListType = ReturnType<typeof editTodoAC>;
+
+export const editTodoAC = (todoId: string, title: string) => {
+  return {
+    type: "EDIT-TODOLIST",
+    payload: {
+      todoId,
+      title,
+    },
+  } as const;
+};
+
+type EditTaskType = ReturnType<typeof editTask>;
+
+export const editTask = (todoId: string, taskId: string, title: string) => {
+  return {
+    type: "EDIT-TASK",
+    payload: {
+      todoId,
+      taskId,
+      title,
+    },
+  } as const;
 };
--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,8 +1,6 @@
 import { v1 } from "uuid";
 import { TodoListType } from "../pages/main/Main";
 import {
-  AddTodoListType,
-  RemoveTodoListType,
   todolistReducer,
 } from "./todolists-reducer";
 
@@ -48,31 +46,3 @@ const startState: TodoListType[] = [
   },
 ];
 
-test("correct todolist should be removed", () => {
-  const action: RemoveTodoListType = {
-    type: "REMOVE-TODOLIST",
-    payload: {
-      todoId: startState[0].id,
-    },
-  };
-
-  const endState = todolistReducer(startState, action);
-
-  expect(endState.length).toBe(1);
-  expect(endState[0].id).toBe(startState[1].id);
-});
-
-test("correct todolist should be added", () => {
-  let newTodolist = "new Todo";
-
-  const action: AddTodoListType = {
-    type: "ADD-TODOLIST",
-    payload: {
-      title: newTodolist,
-    },
-  };
-
-  const endState = todolistReducer(startState, action);
-
-  expect(endState.length).toBe(3);
-});
