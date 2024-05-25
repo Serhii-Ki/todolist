2eb9509b02022a84b1db47d3b905645d839726b2
Kiba Sergey <serhiikiba@gmail.com>
2024-05-25 14:40:46 +0200

    added header and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css'
+import {useState} from "react";
+import {createTheme, ThemeProvider} from "@mui/material";
+import Header from "./components/header/header.tsx";
+
+
+type ThemeModeType = 'light' | 'dark'
 
 function App() {
+  const [themeMode, setThemeMode] = useState<ThemeModeType>('light')
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode === 'light' ? 'light' : 'dark',
+      primary: {
+        main: '#0928a8',
+      },
+    },
+  })
+
+  const changeModeHandler = () => {
+    setThemeMode(themeMode == 'light' ? 'dark' : 'light')
+  }
 
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={theme}>
+      <Header changeModeHandler={changeModeHandler}/>
+    </ ThemeProvider >
   )
 }
 
---
+++ src/components/customBtn/CustomBtn.tsx
@@ -0,0 +1,13 @@
+import Button, {ButtonProps} from '@mui/material/Button';
+
+type ButtonPropsType = {
+  title: string
+} & ButtonProps
+
+function CustomBtn(props: ButtonPropsType) {
+  return (
+      <Button variant="contained">{props.title}</Button>
+  );
+}
+
+export default CustomBtn;
